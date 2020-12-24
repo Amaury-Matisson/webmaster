fc7baafcb8a363aa32256b5f746e47587d40f9d4
Amaury Matisson <amaury.matisson@gmail.com>
2020-12-24 11:34:21 +0100

    commit dark mode

--- js/script.js
+++ js/script.js
@@ -16,3 +16,14 @@ window.onload = function checkDark() {
 		document.body.classList.add("dark");
 	}
 }
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+} 
