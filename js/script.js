6212046941685f70acbb812d1795bbc524028e69
Amaury Matisson <amaury.matisson@gmail.com>
2020-12-24 10:58:59 +0100

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 function darkMode() {
 	document.cookie = "darkmode=1; expires=Wed, 18 Dec 2026 12:00:00 GMT";
-	var x = document.cookie;
-	
-	console.log(x);
+}
+
+function delDarkMode() {
+	document.cookie = "darkmode=; expires=Wed, 18 Dec 2026 12:00:00 GMT";
 }
 
 
 function displayCookies() {
-var fname=getCookie("darkmode");
-if (fname==null) {fname="";}
-if (fname!="") {fname="darkmode="+fname;}
-alert (fname);
+	var fname=getCookie("darkmode");
+	if (fname==null) {fname="";}
+	if (fname!="") {fname="darkmode="+fname;}
+	alert (fname);
 }
 
 function getCookie(name) {
-var nameEQ = name + "=";
-//alert(document.cookie);
-var ca = document.cookie.split(';');
-for(var i=0;i < ca.length;i++) {
-var c = ca[i];
-while (c.charAt(0)==' ') c = c.substring(1);
-if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
-}
-return null;
+	var nameEQ = name + "=";
+	//alert(document.cookie);
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
 } 
